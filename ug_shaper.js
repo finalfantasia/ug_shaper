2ae03562d3c3d83e22f3878637cc527903c836a5
FinalFantasia <finalfantasy@finalfantasia.com>
2013-09-04 22:05:37 -0700

    minor name changes.

--- ug_shaper.js
+++ ug_shaper.js
@@ -25,53 +25,53 @@
     }
 
     function initializeMap() {
-        a2apfMap = {};
-
-        a2apfMap[0x0627] = makeFormSet(0xFE8D, 0xFE8D, 0xFE8D, 0xFE8E, NON_CONNECTABLE); // a
-        a2apfMap[0x06D5] = makeFormSet(0xFEE9, 0xFEE9, 0xFEE9, 0xFEEA, NON_CONNECTABLE); // e
-        a2apfMap[0x0628] = makeFormSet(0xFE8F, 0xFE91, 0xFE92, 0xFE90, CONNECTABLE);     // b
-        a2apfMap[0x067E] = makeFormSet(0xFB56, 0xFB58, 0xFB59, 0xFB57, CONNECTABLE);     // p
-        a2apfMap[0x062A] = makeFormSet(0xFE95, 0xFE97, 0xFE98, 0xFE96, CONNECTABLE);     // t
-        a2apfMap[0x062C] = makeFormSet(0xFE9D, 0xFE9F, 0xFEA0, 0xFE9E, CONNECTABLE);     // j
-        a2apfMap[0x0686] = makeFormSet(0xFB7A, 0xFB7C, 0xFB7D, 0xFB7B, CONNECTABLE);     // ch
-        a2apfMap[0x062E] = makeFormSet(0xFEA5, 0xFEA7, 0xFEA8, 0xFEA6, CONNECTABLE);     // x
-        a2apfMap[0x062F] = makeFormSet(0xFEA9, 0xFEA9, 0xFEAA, 0xFEAA, NON_CONNECTABLE); // d
-        a2apfMap[0x0631] = makeFormSet(0xFEAD, 0xFEAD, 0xFEAE, 0xFEAE, NON_CONNECTABLE); // r
-        a2apfMap[0x0632] = makeFormSet(0xFEAF, 0xFEAF, 0xFEB0, 0xFEB0, NON_CONNECTABLE); // z
-        a2apfMap[0x0698] = makeFormSet(0xFB8A, 0xFB8A, 0xFB8B, 0xFB8B, NON_CONNECTABLE); // zh
-        a2apfMap[0x0633] = makeFormSet(0xFEB1, 0xFEB3, 0xFEB4, 0xFEB2, CONNECTABLE);     // s
-        a2apfMap[0x0634] = makeFormSet(0xFEB5, 0xFEB7, 0xFEB8, 0xFEB6, CONNECTABLE);     // sh
-        a2apfMap[0x063A] = makeFormSet(0xFECD, 0xFECF, 0xFED0, 0xFECE, CONNECTABLE);     // gh
-        a2apfMap[0x0641] = makeFormSet(0xFED1, 0xFED3, 0xFED4, 0xFED2, CONNECTABLE);     // f
-        a2apfMap[0x0642] = makeFormSet(0xFED5, 0xFED7, 0xFED8, 0xFED6, CONNECTABLE);     // q
-        a2apfMap[0x0643] = makeFormSet(0xFED9, 0xFEDB, 0xFEDC, 0xFEDA, CONNECTABLE);     // k
-        a2apfMap[0x06AF] = makeFormSet(0xFB92, 0xFB94, 0xFB95, 0xFB93, CONNECTABLE);     // g
-        a2apfMap[0x06AD] = makeFormSet(0xFBD3, 0xFBD5, 0xFBD6, 0xFBD4, CONNECTABLE);     // ng
-        a2apfMap[0x0644] = makeFormSet(0xFEDD, 0xFEDF, 0xFEE0, 0xFEDE, CONNECTABLE);     // l
-        a2apfMap[0x0645] = makeFormSet(0xFEE1, 0xFEE3, 0xFEE4, 0xFEE2, CONNECTABLE);     // m
-        a2apfMap[0x0646] = makeFormSet(0xFEE5, 0xFEE7, 0xFEE8, 0xFEE6, CONNECTABLE);     // n
-        a2apfMap[0x06BE] = makeFormSet(0xFBAA, 0xFBAC, 0xFBAD, 0xFBAB, CONNECTABLE);     // h
-        a2apfMap[0x0648] = makeFormSet(0xFEED, 0xFEED, 0xFEEE, 0xFEEE, NON_CONNECTABLE); // o
-        a2apfMap[0x06C7] = makeFormSet(0xFBD7, 0xFBD7, 0xFBD8, 0xFBD8, NON_CONNECTABLE); // u
-        a2apfMap[0x06C6] = makeFormSet(0xFBD9, 0xFBD9, 0xFBDA, 0xFBDA, NON_CONNECTABLE); // ö
-        a2apfMap[0x06C8] = makeFormSet(0xFBDB, 0xFBDB, 0xFBDC, 0xFBDC, NON_CONNECTABLE); // ü
-        a2apfMap[0x06CB] = makeFormSet(0xFBDE, 0xFBDE, 0xFBDF, 0xFBDF, NON_CONNECTABLE); // w
-        a2apfMap[0x06D0] = makeFormSet(0xFBE4, 0xFBE6, 0xFBE7, 0xFBE5, CONNECTABLE);     // ë
-        a2apfMap[0x0649] = makeFormSet(0xFEEF, 0xFBE8, 0xFBE9, 0xFEF0, CONNECTABLE);     // i
-        a2apfMap[0x064A] = makeFormSet(0xFEF1, 0xFEF3, 0xFEF4, 0xFEF2, CONNECTABLE);     // y
-        a2apfMap[0x0626] = makeFormSet(0xFE8B, 0xFE8B, 0xFE8C, 0xFB8C, CONNECTABLE);     // hemze
+        a2apfMap = {
+            0x0627: makeFormSet(0xFE8D, 0xFE8D, 0xFE8D, 0xFE8E, NON_CONNECTABLE), // a
+            0x06D5: makeFormSet(0xFEE9, 0xFEE9, 0xFEE9, 0xFEEA, NON_CONNECTABLE), // e
+            0x0628: makeFormSet(0xFE8F, 0xFE91, 0xFE92, 0xFE90, CONNECTABLE),     // b
+            0x067E: makeFormSet(0xFB56, 0xFB58, 0xFB59, 0xFB57, CONNECTABLE),     // p
+            0x062A: makeFormSet(0xFE95, 0xFE97, 0xFE98, 0xFE96, CONNECTABLE),     // t
+            0x062C: makeFormSet(0xFE9D, 0xFE9F, 0xFEA0, 0xFE9E, CONNECTABLE),     // j
+            0x0686: makeFormSet(0xFB7A, 0xFB7C, 0xFB7D, 0xFB7B, CONNECTABLE),     // ch
+            0x062E: makeFormSet(0xFEA5, 0xFEA7, 0xFEA8, 0xFEA6, CONNECTABLE),     // x
+            0x062F: makeFormSet(0xFEA9, 0xFEA9, 0xFEAA, 0xFEAA, NON_CONNECTABLE), // d
+            0x0631: makeFormSet(0xFEAD, 0xFEAD, 0xFEAE, 0xFEAE, NON_CONNECTABLE), // r
+            0x0632: makeFormSet(0xFEAF, 0xFEAF, 0xFEB0, 0xFEB0, NON_CONNECTABLE), // z
+            0x0698: makeFormSet(0xFB8A, 0xFB8A, 0xFB8B, 0xFB8B, NON_CONNECTABLE), // zh
+            0x0633: makeFormSet(0xFEB1, 0xFEB3, 0xFEB4, 0xFEB2, CONNECTABLE),     // s
+            0x0634: makeFormSet(0xFEB5, 0xFEB7, 0xFEB8, 0xFEB6, CONNECTABLE),     // sh
+            0x063A: makeFormSet(0xFECD, 0xFECF, 0xFED0, 0xFECE, CONNECTABLE),     // gh
+            0x0641: makeFormSet(0xFED1, 0xFED3, 0xFED4, 0xFED2, CONNECTABLE),     // f
+            0x0642: makeFormSet(0xFED5, 0xFED7, 0xFED8, 0xFED6, CONNECTABLE),     // q
+            0x0643: makeFormSet(0xFED9, 0xFEDB, 0xFEDC, 0xFEDA, CONNECTABLE),     // k
+            0x06AF: makeFormSet(0xFB92, 0xFB94, 0xFB95, 0xFB93, CONNECTABLE),     // g
+            0x06AD: makeFormSet(0xFBD3, 0xFBD5, 0xFBD6, 0xFBD4, CONNECTABLE),     // ng
+            0x0644: makeFormSet(0xFEDD, 0xFEDF, 0xFEE0, 0xFEDE, CONNECTABLE),     // l
+            0x0645: makeFormSet(0xFEE1, 0xFEE3, 0xFEE4, 0xFEE2, CONNECTABLE),     // m
+            0x0646: makeFormSet(0xFEE5, 0xFEE7, 0xFEE8, 0xFEE6, CONNECTABLE),     // n
+            0x06BE: makeFormSet(0xFBAA, 0xFBAC, 0xFBAD, 0xFBAB, CONNECTABLE),     // h
+            0x0648: makeFormSet(0xFEED, 0xFEED, 0xFEEE, 0xFEEE, NON_CONNECTABLE), // o
+            0x06C7: makeFormSet(0xFBD7, 0xFBD7, 0xFBD8, 0xFBD8, NON_CONNECTABLE), // u
+            0x06C6: makeFormSet(0xFBD9, 0xFBD9, 0xFBDA, 0xFBDA, NON_CONNECTABLE), // ö
+            0x06C8: makeFormSet(0xFBDB, 0xFBDB, 0xFBDC, 0xFBDC, NON_CONNECTABLE), // ü
+            0x06CB: makeFormSet(0xFBDE, 0xFBDE, 0xFBDF, 0xFBDF, NON_CONNECTABLE), // w
+            0x06D0: makeFormSet(0xFBE4, 0xFBE6, 0xFBE7, 0xFBE5, CONNECTABLE),     // ë
+            0x0649: makeFormSet(0xFEEF, 0xFBE8, 0xFBE9, 0xFEF0, CONNECTABLE),     // i
+            0x064A: makeFormSet(0xFEF1, 0xFEF3, 0xFEF4, 0xFEF2, CONNECTABLE),     // y
+            0x0626: makeFormSet(0xFE8B, 0xFE8B, 0xFE8C, 0xFB8C, CONNECTABLE)     // hemze
+        };
     }
 
     function convert(inputString) {
         var convertedChars = [],
             currentChar, previousChar,
             currentFormSet, previousFormSet,
-            currentConnectivity, previousConnectivity = NON_CONNECTABLE,
             currentPresentationForm, previousPresentationForm,
+            currentConnectivity, previousConnectivity = NON_CONNECTABLE,
             i;
 
         if (typeof inputString !== 'string') {
-            throw 'Exception: expected input type of "string", instead got ' + (typeof inputString);
+            throw 'Expected input of type "string", instead got ' + (typeof inputString);
         }
 
         if (!a2apfMap) {
@@ -85,8 +85,8 @@
             if (currentChar >= ARABIC_START && currentChar < ARABIC_END && currentFormSet) {
                 // previous letter was a connectable Uyghur letter.
                 if (previousConnectivity === CONNECTABLE) { 
-                    // change previous presentation form so that it gets presented as connected to the current one.
-                    if (currentChar === 0x0627 && previousChar === 0x0644) { // special cases for LA and _LA
+                    // special cases for LA and _LA
+                    if (currentChar === 0x0627 && previousChar === 0x0644) {
                         if (previousPresentationForm === a2apfMap[0x0644].isolated) {
                             previousPresentationForm = LA;
                         } else { // final form
@@ -96,16 +96,17 @@
                         currentPresentationForm = '';
                         currentConnectivity = NON_CONNECTABLE;
                     } else { // general case
-                        if (previousPresentationForm === previousFormSet.isolated) {
+                        if (previousPresentationForm === previousFormSet.isolated) { // A changes to A_
                             previousPresentationForm = previousFormSet.initial;
                         } else { // final form
-                            previousPresentationForm = previousFormSet.medial;
+                            previousPresentationForm = previousFormSet.medial; // _A changes to _A_
                         }
 
-                        currentPresentationForm = currentFormSet.final;
+                        currentPresentationForm = currentFormSet.final; // Last character is always final.
                         currentConnectivity = currentFormSet.connectivity;
                     }
 
+                    // change previous presentation form so that it is presented as connected to current one.
                     convertedChars[convertedChars.length - 1] = String.fromCharCode(previousPresentationForm);
                 } else { // previous letter was not a connectable Uyghur letter.
                     currentPresentationForm = currentFormSet.isolated;
@@ -126,7 +127,7 @@
 
         return convertedChars.join('');
     }
-   
+
     function load() {
         if (/(iPhone|iPod|iPad).*AppleWebKit/i.test(window.navigator.userAgent)) {
             window.document.body.innerHTML = convert(window.document.body.innerHTML);
@@ -164,7 +165,7 @@
             }
 
             window.document.addEventListener('DOMContentLoaded', domReadyListener, false);
-            window.addEventListener('load', domReadyListener, false); // Fallback. 
+            window.addEventListener('load', domReadyListener, false); // Fallback.
         }
 
         addDomReadyListener();
